4c48a00100269244088ca0f72050e82919c0dfbe
Sergio <sstuardobonilla@gmail.com>
2026-05-17 21:06:31 -0400

    Inventario

---
+++ ejercicio.py
@@ -0,0 +1,16 @@
+print ("Estes es mi trabajo de la clase de python")
+
+num1 = int(input("Ingrese el primer numero: "))
+num2 = int(input("Ingrese el segundo numero: "))
+
+suma = num1 + num2
+print ("La suma de los dos numeros es: ", suma)
+resta = num1 - num2
+print ("La resta de los dos numeros es: ", resta)
+multiplicacion = num1 * num2
+print ("La multiplicacion de los dos numeros es: ", multiplicacion)
+if num2 != 0:
+    division = num1 / num2
+    print ("La division de los dos numeros es: ", division)
+else:
+    print ("No se puede dividir por cero")       
